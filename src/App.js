176b0851ed259f357a80153b0c3e13a74902539c
Parth Gondaliya <pgondaliya303@gmail.com>
2023-05-01 11:31:26 +0530

    - changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import './App.css';
-import AllRoutes from './Routes/AllRoutes';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import "./App.css";
+import AllRoutes from "./Routes/AllRoutes";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "leaflet/dist/leaflet.css";
+var ReactCSSTransitionGroup = require("react-addons-css-transition-group");
 
 function App() {
   return (
-  <React.Fragment>
-  <AllRoutes/>
-  </React.Fragment>
+    <React.Fragment>
+      <AllRoutes />
+      <ReactCSSTransitionGroup
+        transitionName="example"
+        transitionAppear={true}
+        transitionAppearTimeout={500}
+        transitionEnter={false}
+        transitionLeave={false}
+      >
+      </ReactCSSTransitionGroup>
+    </React.Fragment>
   );
 }
 
--- src/Common/TechCards/TechCards.js
+++ src/Common/TechCards/TechCards.js
@@ -25,38 +25,7 @@ const TechCards = () => {
                 </div>
               </div>
             </div>
-            {/* <div>
-              <div className="boxs">
-                <div className="box py-4">
-                  <img src={person} alt="Jessica" />
 
-                  <h4 className="mt-4">
-                    "Mi suscipit magna facilisis class placerat. Hendrerit
-                    maecenas leo consectetuer neque nascetur. Litora consequat
-                    hac suscipit ad quis tempus imperdiet consectetuer etiam
-                    iaculis."{" "}
-                  </h4>
-                  <h3 className="review-title">Daniel Walker</h3>
-                  <h5 className="review-desc">Founder makko</h5>
-                </div>
-              </div>
-            </div>
-            <div>
-              <div className="boxs">
-                <div className="box py-4">
-                  <img src={person} alt="Jessica" />
-
-                  <h4 className="mt-4">
-                    "Mi suscipit magna facilisis class placerat. Hendrerit
-                    maecenas leo consectetuer neque nascetur. Litora consequat
-                    hac suscipit ad quis tempus imperdiet consectetuer etiam
-                    iaculis."{" "}
-                  </h4>
-                  <h3 className="review-title">Brooke Hawkins</h3>
-                  <h5 className="review-desc">Business Manager</h5>
-                </div>
-              </div>
-            </div> */}
           </div>
         </div>
       </div>
